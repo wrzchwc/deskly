07fca4fa651c78c84a2dac2ae9af99d492ce5713
Jakub Wierzchowiec <wierzchowieckuba@gmail.com>
2025-01-24 10:34:42 +0100

    added booking page prototype

--- modules/booking/src/booking.routes.ts
+++ modules/booking/src/booking.routes.ts
@@ -1,14 +1,10 @@
 import { Routes } from '@angular/router';
-import { BookingFacade } from './lib/domain/booking-facade';
-import { provideEffects } from '@ngrx/effects';
-import { BookingEffects } from './lib/domain/booking.effects';
 
 export const BOOKING_ROUTES: Routes = [
   {
     path: '',
     loadComponent: async () =>
       (await import('./lib/feature/booking-page.component'))
-        .BookingPageComponent,
-    providers: [BookingFacade, provideEffects(BookingEffects)]
+        .BookingPageComponent
   }
 ];
--- modules/booking/src/lib/feature/booking-page.component.ts
+++ modules/booking/src/lib/feature/booking-page.component.ts
@@ -1,22 +1,15 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatIconModule } from '@angular/material/icon';
-import { BookingFacade } from '../domain/booking-facade';
 
 @Component({
   selector: 'deskly-booking-page',
   standalone: true,
-  imports: [CommonModule, MatButtonModule, MatTooltipModule, MatIconModule],
+  imports: [CommonModule, MatButtonModule, MatTooltipModule, MatIconModule, NgOptimizedImage],
   templateUrl: './booking-page.component.html',
   styleUrl: './booking-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class BookingPageComponent {
-  constructor(private readonly bookingFacade: BookingFacade) {}
-
-  bookResource() {
-    this.bookingFacade.bookResource();
-  }
-}
+export class BookingPageComponent {}
